1a5e2a4b6ee9411042d1596df44003dd003492bb
zhaole <763815972@qq.com>
2020-12-27 20:20:03 +0800

    ss

---
+++ TongTest/test中文/TViewController.h
@@ -0,0 +1,17 @@
+//
+//  TViewController.h
+//  TongTest
+//
+//  Created by 乐哥 on 2020/12/27.
+//  Copyright © 2020 赵乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TongTest/test中文/TViewController.m
@@ -0,0 +1,32 @@
+//
+//  TViewController.m
+//  TongTest
+//
+//  Created by 乐哥 on 2020/12/27.
+//  Copyright © 2020 赵乐. All rights reserved.
+//
+
+#import "TViewController.h"
+
+@interface TViewController ()
+
+@end
+
+@implementation TViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
